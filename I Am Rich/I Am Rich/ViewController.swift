2147361247d85e066d4b75c1117645528c9cc3d8
Dan <27363294+Da-Shu@users.noreply.github.com>
2019-07-23 23:04:24 -0400

    I am rich
    mock app of the original "I am Rich" app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Daniel Shumate on 4/30/19.
+//  Copyright © 2019 Daniel Shumate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
